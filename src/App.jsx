6060d2d81a2b814364979d534dfd6e71393993d7
Rob Sassack <rsassack25@gmail.com>
2022-12-05 15:28:31 -0500

    Added gameplay functionality with scorekeeping

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./Card";
 import data from "./data.js";
 import "./App.css";
 
 function App() {
   const [cards, setCards] = useState(data);
+  const [selected, setSelected] = useState([]);
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
 
+  // at start of render, shuffle cards
+  useEffect(() => {
+    randomOrder();
+  }, []);
+
+  function randomOrder() {
+    cards.sort(() => Math.random() - 0.5);
+  }
+
+  function resetGame() {
+    setSelected([]);
+    setScore(0);
+    randomOrder();
+  }
+
+  function clickCard(id) {
+    if (selected.includes(id)) {
+      resetGame();
+    } else {
+      setSelected((prevState) => [...prevState, id]);
+      setScore((prevState) => (prevState + 1));
+      if (score >= highScore) {
+        setHighScore(score + 1);
+      }
+      randomOrder();
+    }
+  }
+
   let cardList = cards.map((card) => {
-    return <Card key={card.id} name={card.name} image={card.image} />;
+    return <Card key={card.id} clickCard={clickCard} card={card} />;
   });
 
   return (
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,8 @@
 function Card(props) {
   return (
-    <div className='card'>
-      <img src={props.image} alt={props.name} />
-      <p>{props.name}</p>
+    <div className='card' onClick={() => props.clickCard(props.card.id)}>
+      <img src={props.card.image} alt={props.card.name} />
+      <p>{props.card.name}</p>
     </div>
   );
 }
